3ecdba76fd9f49886da6b9767c1b205ee25b333c
Jacob <jacob.peat01@gmail.com>
2022-04-18 21:18:54 +0100

    Set up error handling for getBackground function

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-let urlCount = 0
 async function getBackground(){
     const response = await fetch("https://api.unsplash.com/photos/random?client_id=Wg_AeGIbG1-4lntKf27nJRi3ZzGuNCSPMcfPdNix4Mo&orientation=landscape&query=supercars")
-    const data = await response.json()
+    if(!response.ok){
+        throw Error("Something went wrong!")
+    }
+    try{
+        const data = await response.json()
     console.log(data.urls.full)
     document.body.style.backgroundImage = `url(${data.urls.regular})`
-    document.getElementById("author").innerHTML = 
+    document.getElementById("author").innerText = 
     `
         By: ${data.user.name}
     `
+    }
+    catch(error){
+        console.error(error)
+    }
+    
 }
 getBackground()
